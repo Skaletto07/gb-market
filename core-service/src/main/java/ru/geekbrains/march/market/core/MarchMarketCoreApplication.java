641e7d38a6a31c10f8f11795e4bc518007bbd066
Skaletto07 <Skaletto07@yandex.ru>
2023-01-17 20:13:18 +0300

    hw

---
+++ core-service/src/main/java/ru/geekbrains/march/market/core/MarchMarketCoreApplication.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.march.market.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchMarketCoreApplication {
+	// Домашнее задание:
+	// 0. Покрыть МСы тестами, штук 5-... тестов
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarchMarketCoreApplication.class, args);
+	}
+}
