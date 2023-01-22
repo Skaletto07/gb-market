61ab0848aebe738ed30591b02e9808814a3ade44
Skaletto07 <Skaletto07@yandex.ru>
2023-01-22 20:23:05 +0300

    hw

--- cart-service/src/main/java/ru/geekbrains/march/market/cart/utils/Cart.java
+++ cart-service/src/main/java/ru/geekbrains/march/market/cart/utils/Cart.java
@@ -3,12 +3,18 @@ package ru.geekbrains.march.market.cart.utils;
 import lombok.Data;
 import ru.geekbrains.march.market.api.ProductDto;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class Cart {
     private List<CartItem> items;
     private BigDecimal totalPrice;
+
+    public Cart() {
+        this.items = new ArrayList<>();
+        this.totalPrice = BigDecimal.ZERO;
+    }
 
     public void add(ProductDto p) {
         for (CartItem item : items) {
@@ -21,6 +27,12 @@ public class Cart {
         CartItem cartItem = new CartItem(p.getId(), p.getTitle(), 1, p.getPrice(), p.getPrice());
         items.add(cartItem);
         recalculate();
+    }
+
+    public void remove(Long productId) {
+        if (items.removeIf(p -> p.getProductId().equals(productId))) {
+            recalculate();
+        }
     }
 
     public void clear() {
